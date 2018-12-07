88513e17f414e7512f0288a5d34cd9fc042cd3ad
1chor <37573242+1chor@users.noreply.github.com>
2018-12-07 17:02:19 +0100

    started sample-wise operation

--- tools/matlab/reverb_reference.m
+++ tools/matlab/reverb_reference.m
@@ -20,8 +20,10 @@
 
 	% Note that in Matlab/Octave the first index in an array has index ONE!
 
-	fft_length = pow2(13); % returns th Nth power of two
-
+	%fft_length = pow2(13); % returns th Nth power of two
+    N_length = 512; % Length for direct Fir
+    header_length = 256; % Length of Header FDL Blocks
+    body_length = 4096; % Length of Body FDL Blocks
 
 	% for easier processing, make sure that the input signal as well as the imuplse
 	% response signal have a length which is a mulitple of fft_length/2 ()
@@ -30,8 +32,8 @@
 	sprintf("  input file: %d", length(input_signal))
 	sprintf("  ir file: %d", length(ir_signal))
 
-	ir_length = ceil(length(ir_signal)/(fft_length/2))*(fft_length/2);
-	input_length = ceil(length(input_signal)/(fft_length/2))*(fft_length/2);
+	ir_length = ceil(length(ir_signal)/(N_length))*(N_length);
+	input_length = ceil(length(input_signal)/(N_length))*(N_length);
 
 	ir_signal = [ir_signal;zeros(ir_length-length(ir_signal),2)];
 	input_signal = [input_signal;zeros(input_length-length(input_signal),2)];
@@ -43,59 +45,66 @@
 
 	% To perform the convolution using the overlap-add method we chop up the input
 	% signal and the impulse response into chunks of length block_length 
-	fft_length
-	block_length = fft_length/2
+	%fft_length
+	%block_length = fft_length/2
 	num_input_blocks = length(input_signal)/block_length
 	num_ir_blocks = length(ir_signal)/block_length
 
 	% initialize output signal and make it one block longer than the input signal
 	% this is avoids an buffer overflow for the last block
 	output_signal = zeros(length(input_signal)+1*block_length,2);
+    
+    output_buffer_header_1 = zeros(header_length,1);
+    output_buffer_header_2 = zeros(header_length,1);
+    output_buffer_body_1 = zeros(body_length,1);
+    output_buffer_body_2 = zeros(body_length,1);
+    
+    for s=0:input_length-1
+        %Buffer for fir filter
+        input_buffer_fir_1 = buffer(input_signal(s+1,1),N_length);
+        input_buffer_fir_2 = buffer(input_signal(s+1,2),N_length);
+        
+        %Buffer for header FDL
+        input_buffer_header_1 = buffer(input_signal(s+1,1),header_length);
+        input_buffer_header_2 = buffer(input_signal(s+1,2),header_length);
+       
+        %Buffer for body FDL
+        input_buffer_body_1 = buffer(input_signal(s+1,1),body_length);
+        input_buffer_body_2 = buffer(input_signal(s+1,2),body_length);
+        
+        fir_1 = filter(ir_signal(1:513,1),1,input_buffer_fir_1);
+        fir_2 = filter(ir_signal(1:513,2),1,input_buffer_fir_2);
 
-    fir_1 = filter(ir_signal(1:2*block_length,1),1,input_signal(:,1));
-    fir_2 = filter(ir_signal(1:2*block_length,2),1,input_signal(:,2));
-   
-	for i=0:num_input_blocks-1
-		%left channel
-		output_buffer = zeros(fft_length,1);
-		for j=0:num_ir_blocks-1
-			input_block_index = i-j;
-			%at the beginning of the file there is no history yet --> exit loop
-			if(input_block_index < 0)
-				break;
-            end
-			% load the required blocks and zero-extend them to fft_length
-			% rememer that the length of the result of a convolution is 
-			% given by the addition of the lengths of the inputs signals 
-			input_block = [input_signal(1+input_block_index*block_length:(input_block_index+1)*block_length,1);zeros(block_length,1)];
-			ir_block = [ir_signal(1+j*block_length:(j+1)*block_length,1);zeros(block_length,1)];
-			%perform the mulitplication in the freuqency domain
-			
-            if(j>1)
-                output_buffer = output_buffer + fft(input_block) .* fft(ir_block);
-            end
+        if (mod(s,header_length) == 0)
+            %left channel
+
+            % load the required blocks and zero-extend them to fft_length
+            % rememer that the length of the result of a convolution is 
+            % given by the addition of the lengths of the inputs signals 
+            fft_input_header = fft([input_buffer_header_1;zeros(2*header_length,1)]);
+            fft_ir_header = fft([ir_signal(N_length+1:N_length+header_length,1);zeros(2*header_length,1)]);
+
+            %perform the mulitplication in the freuqency domain
+            output_buffer_header_1 = output_buffer_header_1 + fft_input_header .* fft_ir_header;
+
+            
+            %right channel
+            fft_input_header = fft([input_buffer_header_2;zeros(2*header_length,1)]);
+            fft_ir_header = fft([ir_signal(N_length+1:N_length+header_length,2);zeros(2*header_length,1)]);
+
+            %perform the mulitplication in the freuqency domain
+            output_buffer_header_2 = output_buffer_header_2 + fft_input_header .* fft_ir_header;
         end
-		output_buffer = real(ifft(output_buffer)) + [fir_1(1+i*block_length:(i+1)*block_length);zeros(block_length,1)];
-		
-		output_signal(1+i*block_length:(i+2)*block_length,1) = output_buffer + output_signal(1+i*block_length:(i+2)*block_length,1);
-		
-		%right channel
-		output_buffer = zeros(fft_length,1);
-		for j=0:num_ir_blocks-1
-			input_block_index = i-j;
-			if(input_block_index < 0)
-				break;
-            end
-			input_block = [input_signal(1+input_block_index*block_length:(input_block_index+1)*block_length,2);zeros(block_length,1)];
-			ir_block = [ir_signal(1+j*block_length:(j+1)*block_length,2);zeros(block_length,1)];
-			
-            if(j>1)
-                output_buffer = output_buffer + fft(input_block) .* fft(ir_block);
-            end
+        
+        if (mod(s,body_length) == 0)
+            
         end
-		output_buffer = real(ifft(output_buffer)) + [fir_2(1+i*block_length:(i+1)*block_length);zeros(block_length,1)];
-		
-		output_signal(1+i*block_length:(i+2)*block_length,2) = output_buffer + output_signal(1+i*block_length:(i+2)*block_length,2);
+        
+        output_buffer = real(ifft(output_buffer)) + [fir_1(1+s*N_length:(s+1)*N_length);zeros(body_length,1)];
+        output_signal(1+s*N_length:(s+2)*N_length,1) = output_buffer + output_signal(1+s*N_length:(s+2)*N_length,1);
+        
+        output_buffer = real(ifft(output_buffer)) + [fir_2(1+i*block_length:(i+1)*block_length);zeros(block_length,1)];
+        output_signal(1+i*block_length:(i+2)*block_length,2) = output_buffer + output_signal(1+i*block_length:(i+2)*block_length,2);  
     end
    
 	% crop the size of the output_signal to that of the input signal 
@@ -116,19 +125,23 @@
 
 %% Cost Analysis
 
-syms N B; %Creates symbolic variable
-syms k T;
-%k = 1.5; %Proportionality constant
-%T = ir_length; %Length of filter impulse response
+syms B; %Creates symbolic variable
+%syms N k T;
+k = 1.5; %Proportionality constant
+T = ir_length; %Length of filter impulse response
 f_s = 48000; %Samplefrequenz
 f_fpga = 100000000; %Clockfrequenz vom FPGA
 cycle = f_fpga / f_s; %Zeit für direkt FFT
 
-sprintf("Cost of Single-FDL Convolution")
-O_SFDL = 4*k*log2(2*N) + 4*T/N; %Cost of Single-FDL Convolution
-N_opt = solve(diff(O_SFDL,N)==0,N) %Optimal value for Single-FDL Convolution
+fir_cycle = 4;
+
+N = pow2(floor(log2(cycle/fir_cycle)));
+
+% sprintf("Cost of Single-FDL Convolution")
+% O_SFDL = 4*k*log2(2*N) + 4*T/N; %Cost of Single-FDL Convolution
+% N_opt = solve(diff(O_SFDL,N)==0,N) %Optimal value for Single-FDL Convolution
 
 sprintf("Cost of Double-FDL Convolution")
 O_DoubleFDL = 4*k*log2(2*N) + 4*B/N + 4*k*log2(2*B) + 4*(T/B-1); %Cost of Double-FDL Convolution
 B_opt = solve(diff(O_DoubleFDL,B)==0,B) %Optimal value for Double-FDL Convolution
-
+%B_opt = 4096
