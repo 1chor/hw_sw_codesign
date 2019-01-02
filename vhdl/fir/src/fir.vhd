23cbe6d4323ac38e45cbf0a99a127c342177ba4e
1chor <37573242+1chor@users.noreply.github.com>
2019-01-02 21:59:47 +0100

    Changed fir filter
    Now the fir filter use two rams

--- vhdl/fir/src/fir.vhd
+++ vhdl/fir/src/fir.vhd
@@ -7,28 +7,30 @@ use work.math_pkg.all;
 
 entity fir is
 	generic (
-		NUM_COEFFICIENTS : integer := 512; 
-		ADDR_WIDTH       : integer := 9 
+		NUM_COEFFICIENTS : positive := 512; 
+		DATA_WIDTH		 : positive := 32;
+		ADDR_WIDTH       : positive := 9 
 	);
 	port (
 		clk   : in std_logic;
 		res_n : in std_logic;
 		
 		-- streaming sink (input)
-		stin_data  : in std_logic_vector(15 downto 0);
+		stin_data  : in std_logic_vector(DATA_WIDTH-1 downto 0);
 		stin_valid : in std_logic;
 		stin_ready : out std_logic;
 		
 		-- streaming source (output)
-		stout_data   : out std_logic_vector(15 downto 0);
+		stout_data   : out std_logic_vector(DATA_WIDTH-1 downto 0);
 		stout_valid  : out std_logic;
+		stout_ready : in	std_logic; --only needed with backpressure enabled
 		
 		-- memory mapped slave (coefficients)
-		address      : in  std_logic_vector(ADDR_WIDTH-1 downto 0);
-		write        : in  std_logic;
-		read         : in  std_logic;
-		writedata    : in  std_logic_vector(15 downto 0);
-		readdata     : out std_logic_vector(15 downto 0)
+		mm_address      : in  std_logic_vector(ADDR_WIDTH-1 downto 0);
+		mm_write        : in  std_logic;
+		mm_read         : in  std_logic; --not used
+		mm_writedata    : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+		mm_readdata     : out std_logic_vector(DATA_WIDTH-1 downto 0) --not used, coefficients can not be read
 	);
 begin
 	assert (log2c(NUM_COEFFICIENTS) = ADDR_WIDTH) report "Address space is not large enough to hold all coefficients!" severity failure;
@@ -36,96 +38,116 @@ end entity;
 
 architecture arch of fir is
 	
-	type array_type is array (NUM_COEFFICIENTS-1 downto 0) of std_logic_vector(15 downto 0);
-	signal coefficients		 : array_type := (others => (others => '0'));
-	signal coefficients_mult : array_type := (others => (others => '0'));
+	subtype coeff_cnt is integer range 0 to (NUM_COEFFS-1);
+	signal mul_cnt 			  : coeff_cnt;
+    signal mul_cnt_next 	  : coeff_cnt;
+	signal data_addr_oldest 	  : coeff_cnt;
+	signal data_addr_oldest_next : coeff_cnt;
 	
-	signal x_n 		: array_type := (others => (others => '0'));	
-	signal x_n_mult : array_type := (others => (others => '0'));	
+	signal coeff_dout 	 : std_logic_vector(DATA_WIDTH-1 downto 0);
+	signal data_dout 	 : std_logic_vector(DATA_WIDTH-1 downto 0);
 	
-	type mult_type is array (NUM_COEFFICIENTS-1 downto 0) of std_logic_vector(31 downto 0);
-	signal result_mult  : mult_type := (others => (others => '0'));
+	signal coeff_read	 : std_logic;
+	signal data_write	 : std_logic;
+	signal data_read	 : std_logic;
 	
-	type sum_type is array (NUM_COEFFICIENTS-2 downto 0) of std_logic_vector(31 downto 0);
-	signal sum_a		: sum_type := (others => (others => '0'));
-	signal sum_b		: sum_type := (others => (others => '0'));
-	signal sum_result	: sum_type := (others => (others => '0'));
+	signal temp 		 : signed(2*DATA_WIDTH-1 downto 0);
+	signal temp_next	 : signed(2*DATA_WIDTH-1 downto 0);
+	
+	signal coeff_rd_addr : std_logic_vector(ADDR_WIDTH-1 downto 0);
+	signal data_wr_addr  : std_logic_vector(ADDR_WIDTH-1 downto 0);
+	signal data_rd_addr  : std_logic_vector(ADDR_WIDTH-1 downto 0);
 	
 	type state_type is (
 		STATE_IDLE,
 		STATE_MULT,
-		STATE_ADD,
 		STATE_OUTPUT
 	);
 	signal state, state_next : state_type := STATE_IDLE;
-
-	--~ -- Component for multiplier
-	--~ component mult
-		--~ port
-		--~ (
-			--~ dataa  : in std_logic_vector (15 downto 0);
-			--~ datab  : in std_logic_vector (15 downto 0);
-			--~ result : out std_logic_vector (31 downto 0)
-		--~ );
-	--~ end component;
-	
-	--~ -- Component for adder
-	--~ component add
-		--~ port
-		--~ (
-			--~ dataa  : in std_logic_vector (31 downto 0);
-			--~ datab  : in std_logic_vector (31 downto 0);
-			--~ result : out std_logic_vector (31 downto 0)
-		--~ );
-	--~ end component;
 	
 begin
-	-- generate multiplier
-	gen_mult:
-	for i in 0 to NUM_COEFFICIENTS-1 generate
-		mult: entity work.mult
-		port map (
-			x_n_mult(i),
-			coefficients_mult(i),
-			result_mult(i)
-		);
-	end generate gen_mult;
+
+	mm_readdata <= (others => '0'); --default value, not used
 	
-	-- generate adder
-	gen_add:
-	for i in 0 to NUM_COEFFICIENTS-2 generate
-		add: entity work.add
-		port map (
-			sum_a(i),
-			sum_b(i),
-			sum_result(i)
+	--RAM-Block for coefficients
+	ram_coeff: entity work.dp_ram_1c1r1w 
+	generic map (
+		ADDR_WIDTH => ADDR_WIDTH; -- Address bus width
+		DATA_WIDTH => DATA_WIDTH  -- Data bus width
+	);
+	port map (
+		clk => clk; -- Connection for the clock signal.
+		
+		-- read port
+		rd_addr => coeff_rd_addr; -- The address bus for a reader of the dual port RAM.
+		rd_data => coeff_dout; -- The data bus for a reader of the dual port RAM.
+		rd      => coeff_read; -- The indicator signal for a reader of the dual port RAM (must  be set high in order to be able to read).
+		
+		-- write port
+		wr_addr => mm_address; -- The address bus for a writer of the dual port RAM.
+		wr_data => mm_writedata; -- The data bus for a writer of the dual port RAM.
+		wr      => mm_write  -- The indicator signal for a writer of the dual port RAM (must be set high in order to be able to write).
+	);
+	
+	--need to use mul_cnt_next because of one latency cycle inside the RAM
+	coeff_rd_addr <= std_logic_vector(to_unsigned(mul_cnt_next + 1, ADDR_WIDTH));
+	
+	--RAM-Block for data
+	ram_data: entity work.dp_ram_1c1r1w 
+	generic map (
+		ADDR_WIDTH => ADDR_WIDTH;
+		DATA_WIDTH => DATA_WIDTH
+	);
+	port map (
+		clk => clk;
+		
+		-- read port
+		rd_addr => data_rd_addr; 
+		rd_data => data_dout; 
+		rd      => data_read; 
+		
+		-- write port
+		wr_addr => data_wr_addr; 
+		wr_data => ; 
+		wr      => data_write 
 		);
-	end generate gen_add;
+	
+	--need to use mul_cnt_next because of one latency cycle inside the RAM
+    data_wr_addr <= std_logic_vector(to_unsigned(data_addr_oldest, ADDR_WIDTH));
+	data_rd_addr <= std_logic_vector(to_unsigned(data_addr_oldest + mul_cnt_next + 1, ADDR_WIDTH));
 			
 	sync_state_proc: process (res_n, clk)
 	begin
 		if res_n = '0' then -- Reset signals
 			state <= STATE_IDLE;
-			x_n <= (others => (others => '0'));	
+			mul_cnt <= 0;
+			temp <= (others => '0');
+			data_addr_oldest <= 0;
 			
 		elsif rising_edge(clk) then
 			state <= state_next;	
-			
-			--Shift Values
-			if(stin_valid = '1') then
-				x_n(NUM_COEFFICIENTS-1 downto 1) <= x_n(NUM_COEFFICIENTS-2 downto 0);
-				x_n(0) <= stin_data;
-			end if;
+			mul_cnt <= mul_cnt_next;
+			temp <= temp_next;
+			data_addr_oldest <= data_addr_oldest_next;
 		end if;
 			
 	end process sync_state_proc;
 	
-	fir_proc: process (state, stin_valid, x_n, coefficients, result_mult, sum_result)
+	fir_proc: process (all)
 	begin
+		--default values to prevent latches
 		state_next <= state;
+		
+		data_addr_oldest_next <= data_addr_oldest;
+		mul_cnt_next <= 0;
+		temp_next <= temp;
 		
 		stin_ready <= '0';		
 		stout_valid <= '0';
+		coeff_read <= '0';
+		data_write <= '0';
+		data_read <= '0';
+		stout_data <= (others => '0');
 		
 		case state is
 		
@@ -133,39 +155,38 @@ begin
 				stin_ready <= '1'; --Ready for Input
 								
 				if stin_valid = '1' then
-					stin_ready <= '0'; --Activate for simulation
+					data_write <= '1';
 					state_next <= STATE_MULT;
 				end if;
 						
 			when STATE_MULT =>
-				--Multiply
-				--Übergibt die Werte an die Multiplizierer
-				for i in 0 to NUM_COEFFICIENTS-1 loop
-					x_n_mult(i) <= x_n(i);
-					coefficients_mult(i) <= coefficients(i);
-				end loop;
+				coeff_read <= '1';
+				data_read <= '1';
 				
-				state_next <= STATE_ADD;
+				temp_next <= temp + signed(coeff_dout) * signed(data_dout);
 				
-			when STATE_ADD =>
-				--Add
-				--Übergibt die Werte an die Addierer
-				sum_a(0) <= result_mult(0);
-				sum_b(0) <= result_mult(1);
-				
-				for i in 1 to NUM_COEFFICIENTS-2 loop
-					sum_a(i) <= sum_result(i-1);
-					sum_b(i) <= result_mult(i+1);
-				end loop;
-				
-				state_next <= STATE_OUTPUT;
+				if mul_cnt = NUM_COEFFS-1 then --catch overflow
+					state_next <= STATE_OUTPUT;
+				else
+					mul_cnt_next <= mul_cnt + 1;
+				end if;
 				
 			when STATE_OUTPUT =>
-				--Set Output
-				stout_valid <= '1';
-				stout_data <= sum_result(NUM_COEFFICIENTS-2)(23 downto 8);
-				
-				state_next <= STATE_IDLE;
+				if stout_ready = '1' then
+					--Set Output
+					stout_data <= std_logic_vector(temp(47 downto 16));
+					stout_valid <= '1';
+					
+					temp_next <= (others => '0');
+					
+					if data_addr_oldest = 0 then
+						data_addr_oldest_next <= NUM_COEFFS - 1;
+					else
+						data_addr_oldest_next <= data_addr_oldest - 1;
+					end if;
+					
+					state_next <= STATE_IDLE;
+				end if;
 				
 			when others =>
 				state_next <= STATE_IDLE;				
@@ -173,23 +194,23 @@ begin
 			
 	end process fir_proc;
 	
-	coefficient_proc: process (res_n, clk)
-	begin
-		if res_n = '0' then -- Reset signals
-			coefficients <= (others => (others => '0'));
-			readdata <= (others => '0');
+	-- coefficient_proc: process (res_n, clk)
+	-- begin
+		-- if res_n = '0' then -- Reset signals
+			-- coefficients <= (others => (others => '0'));
+			-- readdata <= (others => '0');
 			
-		elsif rising_edge(clk) then
-			if write = '1' then
-				coefficients(to_integer(unsigned(address))) <= writedata;
+		-- elsif rising_edge(clk) then
+			-- if write = '1' then
+				-- coefficients(to_integer(unsigned(address))) <= writedata;
 			
-			elsif read = '1' then
-				readdata <= coefficients(to_integer(unsigned(address)));
+			-- elsif read = '1' then
+				-- readdata <= coefficients(to_integer(unsigned(address)));
 			
-			end if;	
-		end if;
+			-- end if;	
+		-- end if;
 			
-	end process coefficient_proc;
+	-- end process coefficient_proc;
 	
 end architecture;
 
